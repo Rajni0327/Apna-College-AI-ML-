e93c1848c15ef474d0187a3716b1b8895cffcc6e
Rajni Meghwal <meghwalrajni27@gmail.com>
2026-02-03 22:12:34 +0530

    Updating Files

---
+++ pythonFundamental/Part2/code.py
@@ -0,0 +1,60 @@
+# # Conditional Statements
+
+# age = 21 
+# if age >= 18:
+#     print("You can Vote.")
+#     print ("You can drive")
+# else :
+#     print("You cannot Vote.")
+
+
+# color = input ("enter color :")
+
+# if color == "red":
+#     print("stop")
+# elif color == "yellow":
+#     print("get ready")
+# elif color == "green":
+#     print("go")
+# else:
+#     print("invalid color")
+
+
+# age = int(input("Enter your age: "))
+
+# if (age < 13):
+#     print("child")
+# elif (age >= 13 and age < 18):
+#     print("teenager")
+# else:
+#     print("adult")
+
+
+# username = input("Enter username: ")
+# password = input("Enter password: ")
+
+# if username == "admin" and password == "pass":
+#     print("Login successful")
+# elif username != "admin":
+#     print("Invalid username")
+# else :
+#     print("Invalid password")
+
+
+# num = int(input("Enter a number: "))
+
+# if num % 5 == 0:
+#     print("multiple of 5")
+# else:
+#     print("not a multiple of 5")
+
+# #to check if a number is even or odd
+# num = int(input("Enter a number: "))
+
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+#nesting - condition inside another condition
