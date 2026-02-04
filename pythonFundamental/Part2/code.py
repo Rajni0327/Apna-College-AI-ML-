59020a2da5817446a43a4a0776c0876bb0c26fca
Rajni Meghwal <meghwalrajni27@gmail.com>
2026-02-04 19:12:17 +0530

    Updating Files

--- pythonFundamental/Part2/code.py
+++ pythonFundamental/Part2/code.py
@@ -1,4 +1,4 @@
-# # Conditional Statements
+# # Conditional Statements ----------------------------
 
 # age = 21 
 # if age >= 18:
@@ -57,4 +57,104 @@
 #     print("odd")
 
 
-#nesting - condition inside another condition
+#nesting - condition inside another condition ----------------------
+
+# username = input("Enter username: ")
+# password = input("Enter password: ")
+
+# if(username == "admin" and password == "pass"):
+#     print("Login successful")
+# else:
+#     if (username != "admin"):
+#         print("Invalid username")
+#     else :
+#         print("Invalid password")
+
+
+##match case statement ----------------------------
+
+# color = input("Enter color: ")
+
+# match color:
+#     case "Green":
+#         print("Go")
+#     case "Yellow":
+#         print("Get Ready")
+#     case "Red":
+#         print("Stop")
+#     case _:
+#         print("Invalid Color")
+
+
+##loops -----------------------------
+
+##while
+
+# counter = 1   #iterator
+# while (counter <= 5):
+#     print("Hello", counter)
+#     counter += 1
+
+
+# #reverse loop 
+# count = 10 
+# while (count >=1) :
+#     print(count)
+#     count -= 1
+
+
+
+
+##print multiplication table of any number
+
+# n = int(input("Enter a number: "))
+# i = 1
+# while (i <= 10):
+#     print(n*i)
+#     i += 1
+
+
+##break and continue -----------------------------
+
+# i = 1 
+# while (i <= 10):
+#     if (i % 6 == 0):
+#         break
+#     print(i)
+#     i += 1
+
+# print("outside loop now...")
+
+
+
+
+# i = 1
+
+# while (i <= 10):
+#     if ( i % 3 ==0):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# print ("outside loop now...")
+
+
+##print odd numbers 
+
+# i = 1 
+
+# while (i <= 10):
+#     print (i)
+#     i += 2
+# print("outside loop now...")
+
+# i = 0 
+# while (i < 10):
+#     i += 1
+#     if (i % 2 == 0):
+#         continue
+#     print(i)
+
+
+##for loop
