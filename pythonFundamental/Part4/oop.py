bbbcfb5ccb7dd4f95f15802a586d7ddfc375d343
Rajni Meghwal <meghwalrajni27@gmail.com>
2026-02-13 23:11:30 +0530

    updating files

--- pythonFundamental/Part4/oop.py
+++ pythonFundamental/Part4/oop.py
@@ -8,17 +8,36 @@
 
 
 
-# encapsulation
-# wrapping data and functions into single unit
+# # encapsulation
+# # wrapping data and functions into single unit
 
-class BankAccount:
-    def __init__(self, name, balance):
-        self.name = name #public
-        #self._balance = balance  #protected accessed only in classes and sub classes
-        self.__balance = balance  #private -data mangling
+# class BankAccount:
+#     def __init__(self, name, balance):
+#         self.name = name #public
+#         #self._balance = balance  #protected accessed only in classes and sub classes
+#         self.__balance = balance  #private -data mangling
 
-acc1 = BankAccount("Samarth Chauhan", 100_000)
+#     def get_balance(self): #getter function
+#         return self.__balance
 
-print(acc1.name, acc1._balance)
+#     def set_balance(self, newBalance): #setter
+#         self.__balance = newBalance
 
-# in python -when we create protected or private- it is by convention not enforced 
+# acc1 = BankAccount("Samarth Chauhan", 100_000)
+
+# acc1.set_balance(200_000)
+
+# print(acc1.name, acc1.get_balance())
+# # or 
+# print(acc1._BankAccount__balance) #access private value
+
+
+# # in python -when we create protected or private- it is by convention not enforced 
+
+
+
+
+
+# Inheritance
+# resuing attribute and methods from a parent(base) class
+
