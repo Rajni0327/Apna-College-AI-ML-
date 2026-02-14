f9b414e080085557addb6b37ef77b93e3c7391ab
Rajni Meghwal <meghwalrajni27@gmail.com>
2026-02-14 22:03:55 +0530

    updating files

--- pythonFundamental/Part4/oop.py
+++ pythonFundamental/Part4/oop.py
@@ -8,7 +8,7 @@
 
 
 
-# # encapsulation
+# # encapsulation -----------------------------------------------
 # # wrapping data and functions into single unit
 
 # class BankAccount:
@@ -38,28 +38,77 @@
 
 
 
-# Inheritance
-# resuing attribute and methods from a parent(base) class
+# # Inheritance ----------------------------------------------
+# # resuing attribute and methods from a parent(base) class
 
-class Employee:
-    start_time = "10am"
-    end_time = "6pm"
+# class Employee:
+#     start_time = "10am"
+#     end_time = "6pm"
     
-    def change_time(self, new_end_time):
-        self.end_time = new_end_time
+#     def change_time(self, new_end_time):
+#         self.end_time = new_end_time
 
-class Teacher(Employee):
-    def __init__(self, subject):
-        self.subject = subject
+# class Teacher(Employee):
+#     def __init__(self, subject):
+#         self.subject = subject
 
-class AdminStaff(Employee):
-    def __init__(self, role):
-        self.role = role
+# class AdminStaff(Employee):
+#     def __init__(self, role):
+#         self.role = role
 
-t1 = Teacher("Math")
-t1.change_time("5pm")
+# t1 = Teacher("Math")
+# t1.change_time("5pm")
 
-staff1 = AdminStaff("manager")
+# staff1 = AdminStaff("manager")
 
-print(t1.subject, t1.start_time, t1.end_time)
-print(staff1.role, staff1.start_time, staff1.end_time)
+# print(t1.subject, t1.start_time, t1.end_time)
+# print(staff1.role, staff1.start_time, staff1.end_time)
+
+
+# # types
+# # single level inheritance (aove is the example of single inheritance)
+
+# # multi level inheritance
+# class Employee:
+#     start_time = "10am"
+#     end_time = "6pm"
+
+# class AdminStaff(Employee):
+#     def __init__(self, role):
+#         self.role = role
+
+# class Accountant(AdminStaff):
+#         def __init__(self, salary, role):
+#             super().__init__(role)
+#             self.salary = salary
+    
+# acc1 = Accountant(25_000, "CA")
+
+# print(acc1.role, acc1.salary, acc1.start_time, acc1.end_time)
+
+# # multiple inheritance
+
+# # example a student who is a teacher also 
+
+# class Teacher:
+#      def __init__(self, salary):
+#           self.salary = salary
+
+# class Student:
+#      def __init__(self, gpa):
+#           self.gpa = gpa
+
+# class TA(Teacher, Student):
+#      def __init__(self, salary, gpa, name):
+#           super().__init__(salary)
+#           Student.__init__(self, gpa)
+#           self.name = name
+
+# ta1 = TA(15_000, 9.3, "Vishwas")
+
+# print(ta1.name, ta1.gpa, ta1.salary)
+
+
+
+# Abstraction ----------------------------------------
+# means hiding internal details and showing only essential features
